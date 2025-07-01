5da079d6517f730867f92eab5229f4083ee13d53
kangcaleb <calebwkang@gmail.com>
2025-07-01 15:32:39 -0500

    updated logging

--- src/controllers/categorizeController.ts
+++ src/controllers/categorizeController.ts
@@ -5,7 +5,10 @@ export const categorizeEvents = async (req: Request, res: Response) => {
   const events = req.body;
 
   if (!Array.isArray(events)) {
-    return res.status(400).json({ error: "Expected an array of events." });
+    const expectedArrayOfEventsMessage = "Expected an array of events.";
+
+    console.error("❌ AI categorization failed:", expectedArrayOfEventsMessage);
+    return res.status(400).json({ error: expectedArrayOfEventsMessage })
   }
 
   try {
@@ -13,6 +16,6 @@ export const categorizeEvents = async (req: Request, res: Response) => {
     res.json({ categories });
   } catch (error: any) {
     console.error("❌ AI categorization failed:", error.message);
-    res.status(500).json({ error: "AI categorization failed." });
+    res.status(500).json({ error: "AI categorization failed: " + error.message });
   }
 };
