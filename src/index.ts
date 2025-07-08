9429bbd19df4f79db819782b1112d532947776d2
kangcaleb <calebwkang@gmail.com>
2025-07-08 11:24:53 -0400

    increase payload limit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import express from "express";
 import categorizeRoutes from "./routes/routes";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use("/", categorizeRoutes);
 
